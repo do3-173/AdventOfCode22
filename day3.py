ae789b42b0c2c7031c81107e03542bb888948735
do3_173 <edo.fejzic@hotmail.com>
2022-12-04 16:47:59 +0100

    simplify day3

--- day3.py
+++ day3.py
@@ -15,14 +15,9 @@ def first_part():
     array = fi.file_input('day3.txt')
     items = []
     for backpack in array:
-        flag_break = False
         for item in backpack[:(len(backpack) - 1) // 2]:
-            for item_2 in backpack[(len(backpack) - 1) // 2:]:
-                if item == item_2:
-                    items.append(item)
-                    flag_break = True
-                    break
-            if flag_break:
+            if item in backpack[(len(backpack) - 1) // 2:]:
+                items.append(item)
                 break
     return convert(items)
 
@@ -31,17 +26,10 @@ def second_part():
     array = fi.file_input('day3.txt')
     items = []
     for i in range(0, len(array), 3):
-        break_flag = False
-        for items_1 in array[i]:
-            for items_2 in array[i + 1]:
-                if items_1 == items_2:
-                    for items_3 in array[i + 2]:
-                        if items_3 == items_2:
-                            items.append(items_3)
-                            break_flag = True
-                            break
-                if break_flag:
-                    break
+        for item in array[i]:
+            if item in array[i+1] and item in array[i+2]:
+                items.append(item)
+                break
     return convert(items)
 
 
